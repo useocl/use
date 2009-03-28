1ed1f00e3d9ab3e4c3681a6252a729ffa4dc8522
green <green@e95982d2-0d12-e879-a455-8ce5a0885042>
2009-03-28 19:33:05 +0000

    Beginn von VoidType / Set{}
    TODO: 
     - neue leastCommonSuperType-Berechnung.
     - diese in den StdOps überall benutzen
     - Test...
    
    
    git-svn-id: https://db-svn.informatik.uni-bremen.de/repos/stuff/use/trunk@1021 e95982d2-0d12-e879-a455-8ce5a0885042

---
+++ src/main/org/tzi/use/uml/ocl/type/VoidType.java
@@ -0,0 +1,27 @@
+package org.tzi.use.uml.ocl.type;
+
+import java.util.Set;
+
+public class VoidType extends Type {
+
+	public Set allSupertypes() {
+		throw new RuntimeException("Not implemented yet.");
+	}
+
+	public boolean equals(Object obj) {
+		return obj instanceof VoidType;
+	}
+
+	public int hashCode() {
+		return 0;
+	}
+
+	public boolean isSubtypeOf(Type t) {
+		return true;
+	}
+
+	public String toString() {
+		return "OclVoid";
+	}
+
+}
