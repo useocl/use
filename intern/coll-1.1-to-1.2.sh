b32cce2a831e879f29cdf537c03fadc1925e10e5
lhamann <lhamann@e95982d2-0d12-e879-a455-8ce5a0885042>
2010-08-26 10:00:58 +0000

    - Removed old buglist
    git-svn-id: https://db-svn.informatik.uni-bremen.de/repos/stuff/use/trunk@1695 e95982d2-0d12-e879-a455-8ce5a0885042

--- intern/coll-1.1-to-1.2.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-
-mv -f $1 $1.bak
-sed -e 's/com.sun.java.util.collections/java.util/g;' < $1.bak > $1
--- intern/mg-tree2.cmd
+++
@@ -1,9 +0,0 @@
-!create one : TreeNode;
-!set one.name = 'Uno';
-
-!create two : TreeNode;
-!set two.name = 'Due';
-
-!insert (one,two) into Parentship;
-
-!insert (two,one) into Parentship;
