dbebf222a36d8d2d118d64705dd0c1974631d790
lhamann <lhamann@e95982d2-0d12-e879-a455-8ce5a0885042>
2011-10-21 12:51:17 +0000

    - Fixed Bug in Barrier
    git-svn-id: https://db-svn.informatik.uni-bremen.de/repos/stuff/use/trunk@2641 e95982d2-0d12-e879-a455-8ce5a0885042

--- src/main/org/tzi/use/gen/assl/dynamics/GEvalInstructionList.java
+++ src/main/org/tzi/use/gen/assl/dynamics/GEvalInstructionList.java
@@ -60,12 +60,12 @@ public class GEvalInstructionList extends GEvalInstruction implements IGCaller {
 
     protected void proceed(GConfiguration conf,
                          IGCollector collector) throws GEvaluationException {
-        if (fIterator.hasNext() && !collector.getBlocked()) {
+    	if (collector.getBlocked()) {
+    		collector.setBlocked(false);
+    	} else if (fIterator.hasNext()) {
             fIterator.next().createEvalInstr().eval(conf,this,collector);
             fIterator.previous();
-        }
-        else {
-        	collector.setBlocked(false);
+        } else {
             fCaller.feedback( conf, null, collector );
         }
     }
