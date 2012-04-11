de3d403906e34c992d7b11707aba386abd155032
green <green@e95982d2-0d12-e879-a455-8ce5a0885042>
2012-04-11 13:43:11 +0000

    git-svn-id: https://db-svn.informatik.uni-bremen.de/repos/stuff/use/trunk@3208 e95982d2-0d12-e879-a455-8ce5a0885042

---
+++ src/main/org/tzi/use/parser/soil/ast/ASTBlockStatement.java
@@ -0,0 +1,123 @@
+/*
+ * USE - UML based specification environment
+ * Copyright (C) 1999-2010 Mark Richters, University of Bremen
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation; either version 2 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+ */
+
+// $Id: ASTIterationStatement.java 2747 2011-11-25 14:53:17Z lhamann $
+
+package org.tzi.use.parser.soil.ast;
+
+import java.io.PrintWriter;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.tzi.use.parser.SemanticException;
+import org.tzi.use.parser.ocl.ASTExpression;
+import org.tzi.use.parser.ocl.ASTVariableDeclaration;
+import org.tzi.use.uml.ocl.expr.Expression;
+import org.tzi.use.uml.ocl.expr.VarDecl;
+import org.tzi.use.uml.ocl.type.CollectionType;
+import org.tzi.use.uml.ocl.type.Type;
+import org.tzi.use.uml.sys.soil.MBlockStatement;
+import org.tzi.use.uml.sys.soil.MEmptyStatement;
+import org.tzi.use.uml.sys.soil.MIterationStatement;
+import org.tzi.use.uml.sys.soil.MStatement;
+import org.tzi.use.util.StringUtil;
+import org.tzi.use.util.soil.exceptions.CompilationFailedException;
+
+
+/**
+ * AST node of an iteration statement.
+ * @author Daniel Gent
+ *
+ */
+public class ASTBlockStatement extends ASTStatement {
+	
+	/** The variables that are explicitly declared in this block (using 'declare') */
+	private List<ASTVariableDeclaration> fVariableDeclarations;
+	
+	/** The inner statement of this block */
+	private ASTStatement fBody;
+
+	/** Whether explicit variable declarations are required in this (and all inner blocks) */  
+	private boolean fExplicitDeclarations;
+	
+	
+	public ASTBlockStatement(boolean explicitDeclarations) {
+		this.fExplicitDeclarations = explicitDeclarations;
+		this.fVariableDeclarations = new ArrayList<ASTVariableDeclaration>();
+	}
+	
+	public void addVariableDeclaration(ASTVariableDeclaration varDecl) {
+		fVariableDeclarations.add(varDecl);
+	}
+	
+	public void setBody(ASTStatement body) {
+		fBody = body;
+		
+	}
+	
+	@Override
+	protected MStatement generateStatement() throws CompilationFailedException {
+		List<VarDecl> varDecls = new ArrayList<VarDecl>();
+		if (fExplicitDeclarations) {
+			fSymtable.storeState(true);
+			for (ASTVariableDeclaration astVarDecl : fVariableDeclarations) {
+				VarDecl varDecl;
+				try {
+					varDecl = astVarDecl.gen(this.fContext);
+				} catch (SemanticException e) {
+					throw new CompilationFailedException(this, e.getMessage());
+				}
+				if (fSymtable.contains(varDecl.name()))
+					throw new CompilationFailedException(this, "Variable " + varDecl.name() + " already declared");
+	
+				varDecls.add(varDecl);
+				fSymtable.setType(varDecl.name(), varDecl.type());
+			}
+		} 
+		fBoundSet = fBody.bound();
+		fAssignedSet = fBody.assigned();
+		MStatement body = generateStatement(fBody);
+		if (fExplicitDeclarations) {
+			fSymtable.restoreState(this);
+		}
+		return new MBlockStatement(varDecls, body);
+	}
+
+
+	@Override
+	protected void printTree(StringBuilder prelude, PrintWriter target) {
+		target.println(prelude + "[BLOCK]");
+		
+		if (prelude.length() == 0) {
+			prelude.append("+-");
+		} else {
+			prelude.insert(0, "| ");
+		}
+		fBody.printTree(prelude, target);
+		prelude.delete(0, 2);
+	}
+
+
+	@Override
+	public String toString() {
+		//TODO: add implementation for toString()
+		return "TODO";
+	}
+
+}
---
+++ src/main/org/tzi/use/uml/sys/soil/MBlockStatement.java
@@ -0,0 +1,98 @@
+/*
+ * USE - UML based specification environment
+ * Copyright (C) 1999-2010 Mark Richters, University of Bremen
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation; either version 2 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+ */
+
+// $Id: MIterationStatement.java 2747 2011-11-25 14:53:17Z lhamann $
+
+package org.tzi.use.uml.sys.soil;
+
+import java.util.List;
+
+import org.tzi.use.uml.ocl.expr.ExpUndefined;
+import org.tzi.use.uml.ocl.expr.Expression;
+import org.tzi.use.uml.ocl.expr.VarDecl;
+import org.tzi.use.uml.ocl.value.CollectionValue;
+import org.tzi.use.uml.ocl.value.UndefinedValue;
+import org.tzi.use.uml.ocl.value.Value;
+import org.tzi.use.util.soil.exceptions.EvaluationFailedException;
+
+
+/**
+ * TODO
+ * @author Daniel Gent
+ *
+ */
+public class MBlockStatement extends MStatement {
+
+	
+	/** The variables that are explicitly declared in this block (using 'declare') */
+	private List<VarDecl> fVariableDeclarations;
+	
+	/** The inner statement of this block */
+	private MStatement fBody;
+	
+	
+	/**
+	 * TODO
+	 * @param variableName
+	 * @param range
+	 * @param body
+	 */
+	public MBlockStatement(List<VarDecl> varDecls, MStatement body) {
+		fVariableDeclarations = varDecls;
+		fBody = body;
+	}
+	
+
+	@Override
+	protected void evaluate() throws EvaluationFailedException {
+	
+		for (VarDecl varDecl : fVariableDeclarations) {
+			assignVariable(varDecl.name(), UndefinedValue.instance);
+		}
+		evaluateSubStatement(fBody);
+	}
+	
+	
+	@Override
+	protected String shellCommand() {
+		//TODO: implement shellCommand()
+		return "TODO";
+	}
+	
+
+	@Override
+	public boolean hasSideEffects() {
+		return(fBody.hasSideEffects());
+	}
+
+
+	@Override
+	protected void toVisitorString(
+			StringBuilder indent, 
+			String indentIncrease,
+			StringBuilder target) {
+		//TODO: implement toVisitorString()
+	}
+	
+	 
+	@Override
+	public String toString() {
+		return shellCommand();
+	}
+}
