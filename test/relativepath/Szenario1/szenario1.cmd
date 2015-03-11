1192317d34e4d554e1f6b7e7d80e80b3746494fb
fhilken <fhilken@e95982d2-0d12-e879-a455-8ce5a0885042>
2015-03-11 13:45:05 +0000

    - used open instead of read in tests
    git-svn-id: https://db-svn.informatik.uni-bremen.de/repos/stuff/use/trunk@5582 e95982d2-0d12-e879-a455-8ce5a0885042

--- test/relativepath/Szenario1/szenario1.cmd
+++ test/relativepath/Szenario1/szenario1.cmd
@@ -2,57 +2,57 @@ open ../library.use
 
 !create ada:User
 !openter ada init('Ada','NY')
-read ../User/User_init.cmd
+open ../User/User_init.cmd
 !opexit
 
 !create bob:User
 !openter bob init('Bob','CA')
-read ../User/User_init.cmd
+open ../User/User_init.cmd
 !opexit
 
 !create date:Book
 !openter date init('Intro to DBS',Sequence{'Date'},1983)
-read ../Book/Book_init.cmd
+open ../Book/Book_init.cmd
 !opexit
 
 !create dbs42:Copy
 !openter dbs42 init('DBS42',date)
-read ../Copy/Copy_init.cmd
+open ../Copy/Copy_init.cmd
 !opexit
 
 !openter dbs42 borrow(ada)
-read ../Copy/Copy_borrow.cmd
+open ../Copy/Copy_borrow.cmd
 !opexit
 
 !create elNa:Book
 !openter elNa init('Funds of DBS',Sequence{'Elmasri','Navathe'},1994)
-read ../Book/Book_init.cmd
+open ../Book/Book_init.cmd
 !opexit
 
 !create dbs43:Copy
 !openter dbs43 init('DBS43',date)
-read ../Copy/Copy_init.cmd
+open ../Copy/Copy_init.cmd
 !opexit
 
 !create dbs52:Copy
 !openter dbs52 init('DBS52',elNa)
-read ../Copy/Copy_init.cmd
+open ../Copy/Copy_init.cmd
 !opexit
 
 !openter bob borrow(dbs43)
-read ../User/User_borrow.cmd
+open ../User/User_borrow.cmd
 !opexit
 
 !openter dbs52 borrow(ada)
-read ../Copy/Copy_borrow.cmd
+open ../Copy/Copy_borrow.cmd
 !opexit
 
 !openter dbs52 return()
-read ../Copy/Copy_return.cmd
+open ../Copy/Copy_return.cmd
 !opexit
 
 !openter dbs43.user return(dbs43)
-read ../User/User_return.cmd
+open ../User/User_return.cmd
 !opexit
 
 !create ok:OnlyKeyed
