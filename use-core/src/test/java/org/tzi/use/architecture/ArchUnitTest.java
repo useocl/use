cde7c6660fd756bac083b8eaf9b0737dd86da9eb
AliciaMJH <alicia.hagen@haw-hamburg.de>
2024-08-21 11:03:35 +0100

    added ArchUnit Dependency, new Test Class

---
+++ use-core/src/test/java/org/tzi/use/architecture/ArchUnitTest.java
@@ -0,0 +1,23 @@
+package org.tzi.use.architecture;
+
+import com.tngtech.archunit.core.domain.JavaClasses;
+import com.tngtech.archunit.core.importer.ClassFileImporter;
+import com.tngtech.archunit.lang.ArchRule;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import static com.tngtech.archunit.lang.syntax.ArchRuleDefinition.classes;
+import static com.tngtech.archunit.lang.syntax.ArchRuleDefinition.noClasses;
+import static com.tngtech.archunit.library.Architectures.layeredArchitecture;
+
+public class ArchUnitTest {
+
+    private static JavaClasses importedClasses;
+
+    @BeforeAll
+    static void setup() {
+        importedClasses = new ClassFileImporter().importPackages("org.tzi.use");
+    }
+
+
+}
