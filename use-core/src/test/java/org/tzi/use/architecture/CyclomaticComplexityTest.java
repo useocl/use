202b0ee90bccc4e1702d6b93c3ffaaa0756f1667
AliciaMJH <alicia.hagen@haw-hamburg.de>
2024-09-10 22:46:07 +0200

    renamed class & wrote method to count cycles

--- use-core/src/test/java/org/tzi/use/architecture/CyclomaticComplexityTest.java
+++ use-core/src/test/java/org/tzi/use/architecture/CyclomaticComplexityTest.java
@@ -11,7 +11,9 @@ import com.tngtech.archunit.library.dependencies.SliceIdentifier;
 import com.tngtech.archunit.library.dependencies.SlicesRuleDefinition;
 import static com.tngtech.archunit.library.metrics.MetricsComponents.fromPackages;
 
-public class CyclomaticDependencyTest {
+import java.util.concurrent.atomic.AtomicInteger;
+
+public class CyclomaticComplexityTest {
 
     private final JavaClasses classes = new ClassFileImporter().importPackages("org.tzi.use");
 
@@ -53,4 +55,50 @@ public class CyclomaticDependencyTest {
         SlicesRuleDefinition.slices().assignedFrom(sliceAssignment).should().beFreeOfCycles().check(classes);
     }
 
+    @Test
+    public void count_cycles_in_core() {
+        countCyclesForPackage("org.tzi.use");
+    }
+
+    @Test
+    void count_cycles_in_uml_package() {
+        countCyclesForPackage("org.tzi.use.uml");
+    }
+
+    // maybe it's better to have this function actually return the number, not be void
+    private void countCyclesForPackage(String packageName) {
+        SliceAssignment sliceAssignment = new SliceAssignment() {
+            @Override
+            public SliceIdentifier getIdentifierOf(JavaClass javaClass) {
+                if (javaClass.getPackageName().startsWith(packageName)) {
+                    String subPackage = javaClass.getPackageName().substring(packageName.length());
+                    if (subPackage.isEmpty()) {
+                        return SliceIdentifier.of("root");
+                    }
+
+                    String[] parts = subPackage.substring(1).split("\\.");
+                    return SliceIdentifier.of(parts.length > 0 ? parts[0] : "root");
+                }
+                return SliceIdentifier.ignore();
+            }
+
+            @Override
+            public String getDescription() {
+                return "Slices for " + packageName;
+            }
+        };
+
+        AtomicInteger cycleCount = new AtomicInteger(0);
+
+        SlicesRuleDefinition.slices()
+            .assignedFrom(sliceAssignment)
+            .should().beFreeOfCycles()
+            .allowEmptyShould(true)
+            .evaluate(classes)
+            .handleViolations((violatingObjects, violationHandler) -> {
+                cycleCount.incrementAndGet();
+                System.out.println("Cycle found: " + violatingObjects.iterator().next().toString());
+            });
+        System.out.println("Number of cycles in " + packageName + ": " + cycleCount);
+    }
 }
