5a822bf9895a69e308ccbcd157bf47190e228aef
Daniel Moritz <daniel.moritz@haw-hamburg.de>
2025-02-25 22:10:57 +0100

    configure the merging of two StyleInfoClassNodes

--- use-gui/src/main/java/org/tzi/use/gui/views/diagrams/StyleInfoClassNode.java
+++ use-gui/src/main/java/org/tzi/use/gui/views/diagrams/StyleInfoClassNode.java
@@ -2,14 +2,18 @@ package org.tzi.use.gui.views.diagrams;
 
 import lombok.Builder;
 import lombok.Getter;
+import org.tzi.use.gui.views.diagrams.classdiagram.ClassNode;
 
 import java.awt.*;
 import java.util.Optional;
 
 @Getter
 public class StyleInfoClassNode extends StyleInfoBase {
+
     private Color backgroundColor;
+
     private final Color[] attributeColor;
+
     private final Color[] operationColor;
 
     @Builder(setterPrefix = "with")
@@ -20,31 +24,55 @@ public class StyleInfoClassNode extends StyleInfoBase {
         this.operationColor = operationColor;
     }
 
-    public StyleInfoClassNode merge(final StyleInfoClassNode other) {
-        // prioritizes changes of other
-        this.namesColor = Optional.ofNullable(other.getNamesColor()).orElse(this.namesColor);
-        this.backgroundColor = Optional.ofNullable(other.getBackgroundColor()).orElse(this.backgroundColor);
-        mergeAttributeColors(other.getAttributeColor());
-        mergeOperationColors(other.operationColor);
-        return this;
+    /**
+     * Creates a {@link StyleInfoClassNode} from an existing {@link ClassNode}.
+     *
+     * @param classNode the {@link ClassNode} to take the information from
+     * @return {@link StyleInfoClassNode} with the information of the given {@link ClassNode}
+     */
+    public static StyleInfoClassNode createFromClassNode(final ClassNode classNode) {
+        final Color[] attributeColor = classNode.cls().attributes().stream().map(classNode::getAttributeColor).toList().toArray(new Color[0]);
+        final Color[] operationColor = classNode.cls().operations().stream().map(classNode::getOperationColor).toList().toArray(new Color[0]);
+        return new StyleInfoClassNode(classNode.getTextColor(), classNode.getBackColor(), attributeColor, operationColor);
     }
 
-    public StyleInfoClassNode mergeAttributeColors(Color[] otherAttributeColors) {
-        // prioritizes changes of other
+    /**
+     * Merges the information of two {@link StyleInfoClassNode}
+     * This method prioritizes the {@link StyleInfoClassNode} it has been called on.
+     *
+     * @param other the other {@link StyleInfoClassNode}
+     */
+    public void merge(final StyleInfoClassNode other) {
+        this.namesColor = Optional.ofNullable(this.namesColor).orElse(other.getNamesColor());
+        this.backgroundColor = Optional.ofNullable(this.backgroundColor).orElse(other.getBackgroundColor());
+        mergeAttributeColors(other.getAttributeColor());
+        mergeOperationColors(other.getOperationColor());
+    }
+
+    /**
+     * Merges the attribute colors fo two {@link StyleInfoClassNode}s.
+     * This method prioritizes the {@link StyleInfoClassNode} it has been called on.
+     *
+     * @param otherAttributeColors the other attribute colors
+     */
+    private void mergeAttributeColors(final Color[] otherAttributeColors) {
         assert this.attributeColor.length == otherAttributeColors.length;
         for (int i = 0; i < this.attributeColor.length; i++) {
-            this.attributeColor[i] = Optional.ofNullable(otherAttributeColors[i]).orElse(this.attributeColor[i]);
+            this.attributeColor[i] = Optional.ofNullable(this.attributeColor[i]).orElse(otherAttributeColors[i]);
         }
-        return this;
     }
 
-    public StyleInfoClassNode mergeOperationColors(Color[] otherOperationColors) {
-        // prioritizes changes of other
+    /**
+     * Merges the operation colors fo two {@link StyleInfoClassNode}s.
+     * This method prioritizes the {@link StyleInfoClassNode} it has been called on.
+     *
+     * @param otherOperationColors the other operation colors
+     */
+    private void mergeOperationColors(final Color[] otherOperationColors) {
         assert this.operationColor.length == otherOperationColors.length;
         for (int i = 0; i < this.operationColor.length; i++) {
-            this.operationColor[i] = Optional.ofNullable(otherOperationColors[i]).orElse(this.operationColor[i]);
+            this.operationColor[i] = Optional.ofNullable(this.operationColor[i]).orElse(otherOperationColors[i]);
         }
-        return this;
     }
 }
 
