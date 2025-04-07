d7e09fb629dca272885e605cd63d964e510ea755
AliciaMJH <alicia.hagen@haw-hamburg.de>
2025-04-07 11:24:23 +0100

    changed forEach to for loop

--- use-gui/src/test/java/org/tzi/use/architecture/AntLayeredArchitectureTest.java
+++ use-gui/src/test/java/org/tzi/use/architecture/AntLayeredArchitectureTest.java
@@ -39,10 +39,10 @@ public class AntLayeredArchitectureTest {
         if (violationCount > 0) {
             System.out.println("\nViolation details:");
             //not compatible with java 6
-            result.getFailureReport().getDetails().forEach(System.out::println);
-//            for (String detail : result.getFailureReport().getDetails()) {
-//                System.err.println(detail);
-//            }
+            //result.getFailureReport().getDetails().forEach(System.out::println);
+            for (String detail : result.getFailureReport().getDetails()) {
+                System.out.println(detail);
+            }
         }
     }
 }
