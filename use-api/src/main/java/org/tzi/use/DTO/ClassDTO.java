03edbb3774e40463d3709dcf7d1b07e9d1bb4fd5
husakki <hueschromo@gmail.com>
2025-10-05 19:42:36 +0200

    clean up the dtos and better classdto

--- use-api/src/main/java/org/tzi/use/DTO/ClassDTO.java
+++ use-api/src/main/java/org/tzi/use/DTO/ClassDTO.java
@@ -1,66 +1,17 @@
 package org.tzi.use.DTO;
 
-import org.springframework.data.annotation.Id;
-import org.springframework.data.mongodb.core.mapping.Document;
-import org.springframework.data.mongodb.core.mapping.DBRef;
-import lombok.Data;
-import lombok.Builder;
-import lombok.NoArgsConstructor;
-import lombok.AllArgsConstructor;
+import lombok.*;
 
 import java.util.ArrayList;
 import java.util.List;
 
-@Document
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
-@Builder
 public class ClassDTO {
 
-    @Id
-    private String id;
     private String name;
-
-    @DBRef
-    @Builder.Default
-    private List<AttributeDTO> attributeDTOS = new ArrayList<>();
-
-    @DBRef
-    @Builder.Default
-    private List<OperationDTO> operationDTOS = new ArrayList<>();
-
-    public void addAttribute(AttributeDTO attributeDTO) {
-        attributeDTOS.add(attributeDTO);
-    }
-
-    public void removeAttribute(AttributeDTO attributeDTO) {
-        attributeDTOS.remove(attributeDTO);
-    }
-
-    public void addOperation(OperationDTO operationDTO) {
-        operationDTOS.add(operationDTO);
-    }
-
-    public void removeOperation(OperationDTO operationDTO) {
-        operationDTOS.remove(operationDTO);
-    }
-
-    // Additional constructor
-    public ClassDTO(String id, String name, AttributeDTO attributes, OperationDTO operations) {
-        this.id = id;
-        this.name = name;
-        this.attributeDTOS = new ArrayList<>();
-        this.operationDTOS = new ArrayList<>();
-        if (attributes != null) this.attributeDTOS.add(attributes);
-        if (operations != null) this.operationDTOS.add(operations);
-    }
-
-    // Convenience constructor
-    public ClassDTO(String id, String name) {
-        this.id = id;
-        this.name = name;
-        this.attributeDTOS = new ArrayList<>();
-        this.operationDTOS = new ArrayList<>();
-    }
+    private List<AttributeDTO> attributes = new ArrayList<>();
+    private List<OperationDTO> operations = new ArrayList<>();
+    private List<AssociationDTO> associations = new ArrayList<>();
 }
--- use-api/src/main/java/org/tzi/use/DTO/ModelDTO.java
+++ use-api/src/main/java/org/tzi/use/DTO/ModelDTO.java
@@ -7,16 +7,12 @@ import java.util.List;
 
 @Data
 @AllArgsConstructor
-//@RequiredArgsConstructor
 @NoArgsConstructor
 public class ModelDTO {
     private String name;
-//    private String id;
     private List<ClassDTO> classes = new ArrayList<>();
     private List<String> associations = new ArrayList<>();
     private List<String> invariants = new ArrayList<>();
-//    private List<PrePostConditionDTO> preConditions = new ArrayList<>();
-//    private List<PrePostConditionDTO> postConditions = new ArrayList<>();
 
     public ModelDTO(String name) {
         this.name = name;
