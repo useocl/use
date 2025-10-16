92cd984a10e19f90c837d75a07bdc3b4f2f2c929
husakki <hueschromo@gmail.com>
2025-10-17 01:34:16 +0200

    deprecated unit tests
    need to be removed soon

--- use-api/src/test/java/org/tzi/use/RestApiControllerTest.java
+++ use-api/src/test/java/org/tzi/use/RestApiControllerTest.java
@@ -8,6 +8,7 @@ import org.junit.jupiter.api.Test;
 import static io.restassured.RestAssured.*;
 import static org.hamcrest.Matchers.*;
 
+@Deprecated
 public class RestApiControllerTest {
 //
 //    @BeforeAll
