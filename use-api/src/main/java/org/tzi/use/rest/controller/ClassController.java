6109699e076b7c1d78adc443d2c501309de47812
husakki <hueschromo@gmail.com>
2025-11-10 20:34:09 +0100

    class endpoints

--- use-api/src/main/java/org/tzi/use/rest/controller/ClassController.java
+++ use-api/src/main/java/org/tzi/use/rest/controller/ClassController.java
@@ -1,25 +1,135 @@
 package org.tzi.use.rest.controller;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.tzi.use.DTO.AttributeDTO;
+import org.tzi.use.DTO.ClassDTO;
+import org.tzi.use.DTO.OperationDTO;
+import org.tzi.use.DTO.PrePostConditionDTO;
 import org.tzi.use.rest.services.ClassService;
+import org.springframework.hateoas.EntityModel;
+import org.springframework.hateoas.CollectionModel;
+import org.springframework.hateoas.server.mvc.WebMvcLinkBuilder;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn;
 
 @RestController
-@RequestMapping("/api")
+@RequestMapping("/api/models/{modelName}")
 @RequiredArgsConstructor
 public class ClassController {
     private final ClassService classService;
 
     @GetMapping("/class/{className}")
-    @GetMapping("/class/attributes")
-    @GetMapping("/class/operations")
-    @GetMapping("/class/preposts")
+    public ResponseEntity<EntityModel<ClassDTO>> getClassByName(@PathVariable String modelName, @PathVariable String className) {
+        ClassDTO classDTO = classService.getClassByName(className);
+        EntityModel<ClassDTO> entityModel = EntityModel.of(classDTO);
+        entityModel.add(linkTo(methodOn(ClassController.class).getClassByName(modelName, className)).withSelfRel());
+        entityModel.add(linkTo(methodOn(ClassController.class).getAttributes(modelName, className)).withRel("attributes"));
+        entityModel.add(linkTo(methodOn(ClassController.class).getOperations(modelName, className)).withRel("operations"));
+        entityModel.add(linkTo(methodOn(ClassController.class).getPrePostConditions(modelName, className)).withRel("preposts"));
+        return ResponseEntity.ok(entityModel);
+    }
+
+    @GetMapping("/class/{className}/attributes")
+    public ResponseEntity<CollectionModel<EntityModel<AttributeDTO>>> getAttributes(@PathVariable String modelName, @PathVariable String className) {
+        List<AttributeDTO> attributes = classService.getAttributes(className);
+        List<EntityModel<AttributeDTO>> attributeModels = attributes.stream()
+                .map(attr -> EntityModel.of(attr,
+                        linkTo(methodOn(ClassController.class).getAttributeByName(modelName, className, attr.getName())).withSelfRel()))
+                .collect(Collectors.toList());
+
+        CollectionModel<EntityModel<AttributeDTO>> collectionModel = CollectionModel.of(attributeModels);
+        collectionModel.add(linkTo(methodOn(ClassController.class).getAttributes(modelName, className)).withSelfRel());
+        return ResponseEntity.ok(collectionModel);
+    }
+
+    @GetMapping("/class/{className}/attribute/{attributeName}")
+    public ResponseEntity<EntityModel<AttributeDTO>> getAttributeByName(@PathVariable String modelName, @PathVariable String className, @PathVariable String attributeName) {
+        // Assuming ClassService has a method to get a single attribute by name
+        AttributeDTO attribute = classService.getAttributeByName(className, attributeName);
+        EntityModel<AttributeDTO> entityModel = EntityModel.of(attribute);
+        entityModel.add(linkTo(methodOn(ClassController.class).getAttributeByName(modelName, className, attributeName)).withSelfRel());
+        return ResponseEntity.ok(entityModel);
+    }
+
+
+    @GetMapping("/class/{className}/operations")
+    public ResponseEntity<CollectionModel<EntityModel<OperationDTO>>> getOperations(@PathVariable String modelName, @PathVariable String className) {
+        List<OperationDTO> operations = classService.getOperations(className);
+        List<EntityModel<OperationDTO>> operationModels = operations.stream()
+                .map(op -> EntityModel.of(op,
+                        linkTo(methodOn(ClassController.class).getOperationByName(modelName, className, op.getOperationName())).withSelfRel()))
+                .collect(Collectors.toList());
+
+        CollectionModel<EntityModel<OperationDTO>> collectionModel = CollectionModel.of(operationModels);
+        collectionModel.add(linkTo(methodOn(ClassController.class).getOperations(modelName, className)).withSelfRel());
+        return ResponseEntity.ok(collectionModel);
+    }
+
+    @GetMapping("/class/{className}/operation/{operationName}")
+    public ResponseEntity<EntityModel<OperationDTO>> getOperationByName(@PathVariable String modelName, @PathVariable String className, @PathVariable String operationName) {
+        // Assuming ClassService has a method to get a single operation by name
+        OperationDTO operation = classService.getOperationByName(className, operationName);
+        EntityModel<OperationDTO> entityModel = EntityModel.of(operation);
+        entityModel.add(linkTo(methodOn(ClassController.class).getOperationByName(modelName, className, operationName)).withSelfRel());
+        return ResponseEntity.ok(entityModel);
+    }
+
+    @GetMapping("/class/{className}/preposts")
+    public ResponseEntity<CollectionModel<EntityModel<PrePostConditionDTO>>> getPrePostConditions(@PathVariable String modelName, @PathVariable String className) {
+        List<PrePostConditionDTO> conditions = classService.getPrePostConditions(className);
+        List<EntityModel<PrePostConditionDTO>> conditionModels = conditions.stream()
+                .map(cond -> EntityModel.of(cond,
+                        linkTo(methodOn(ClassController.class).getPrePostConditionByName(modelName, className, cond.getName())).withSelfRel()))
+                .collect(Collectors.toList());
+
+        CollectionModel<EntityModel<PrePostConditionDTO>> collectionModel = CollectionModel.of(conditionModels);
+        collectionModel.add(linkTo(methodOn(ClassController.class).getPrePostConditions(modelName, className)).withSelfRel());
+        return ResponseEntity.ok(collectionModel);
+    }
+
+    @GetMapping("/class/{className}/prepost/{conditionName}")
+    public ResponseEntity<EntityModel<PrePostConditionDTO>> getPrePostConditionByName(@PathVariable String modelName, @PathVariable String className, @PathVariable String conditionName) {
+        // Assuming ClassService has a method to get a single pre/post condition by name
+        PrePostConditionDTO condition = classService.getPrePostConditionByName(className, conditionName);
+        EntityModel<PrePostConditionDTO> entityModel = EntityModel.of(condition);
+        entityModel.add(linkTo(methodOn(ClassController.class).getPrePostConditionByName(modelName, className, conditionName)).withSelfRel());
+        return ResponseEntity.ok(entityModel);
+    }
+
 
     @PostMapping("/class/{className}/attribute")
-    @PostMapping("/class/{className}/operation")
-    @PostMapping("/class/{className}/prepost")
+    public ResponseEntity<EntityModel<AttributeDTO>> addAttribute(@PathVariable String modelName, @PathVariable String className, @RequestBody AttributeDTO attributeDTO) {
+        AttributeDTO newAttribute = classService.addAttribute(className, attributeDTO);
+        EntityModel<AttributeDTO> entityModel = EntityModel.of(newAttribute);
+        entityModel.add(linkTo(methodOn(ClassController.class).getAttributeByName(modelName, className, newAttribute.getName())).withSelfRel());
+        return new ResponseEntity<>(entityModel, HttpStatus.CREATED);
+    }
 
+    @PostMapping("/class/{className}/operation")
+    public ResponseEntity<EntityModel<OperationDTO>> addOperation(@PathVariable String modelName, @PathVariable String className, @RequestBody OperationDTO operationDTO) {
+        OperationDTO newOperation = classService.addOperation(className, operationDTO);
+        EntityModel<OperationDTO> entityModel = EntityModel.of(newOperation);
+        entityModel.add(linkTo(methodOn(ClassController.class).getOperationByName(modelName, className, newOperation.getOperationName())).withSelfRel());
+        return new ResponseEntity<>(entityModel, HttpStatus.CREATED);
+    }
+
+    @PostMapping("/class/{className}/prepost")
+    public ResponseEntity<EntityModel<PrePostConditionDTO>> addPrePostCondition(@PathVariable String modelName, @PathVariable String className, @RequestBody PrePostConditionDTO prePostConditionDTO) {
+        PrePostConditionDTO newCondition = classService.addPrePostCondition(className, prePostConditionDTO);
+        EntityModel<PrePostConditionDTO> entityModel = EntityModel.of(newCondition);
+        entityModel.add(linkTo(methodOn(ClassController.class).getPrePostConditionByName(modelName, className, newCondition.getName())).withSelfRel());
+        return new ResponseEntity<>(entityModel, HttpStatus.CREATED);
+    }
 }
--- use-api/src/main/java/org/tzi/use/rest/services/ClassService.java
+++ use-api/src/main/java/org/tzi/use/rest/services/ClassService.java
@@ -2,10 +2,56 @@ package org.tzi.use.rest.services;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.tzi.use.DTO.AttributeDTO;
+import org.tzi.use.DTO.ClassDTO;
+import org.tzi.use.DTO.OperationDTO;
+import org.tzi.use.DTO.PrePostConditionDTO;
 import org.tzi.use.repository.ClassRepo;
+
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
 public class ClassService {
     private final ClassRepo classRepo;
+
+    public ClassDTO getClassByName(String className) {
+        return null;
+    }
+
+    public List<AttributeDTO> getAttributes(String className) {
+        return null;
+    }
+
+    public List<OperationDTO> getOperations(String className) {
+        return null;
+    }
+
+    public List<PrePostConditionDTO> getPrePostConditions(String className) {
+        return null;
+    }
+
+    public AttributeDTO addAttribute(String className, AttributeDTO attributeDTO) {
+        return null;
+    }
+
+    public OperationDTO addOperation(String className, OperationDTO operationDTO) {
+        return null;
+    }
+
+    public PrePostConditionDTO addPrePostCondition(String className, PrePostConditionDTO prePostCondition) {
+        return null;
+    }
+
+    public PrePostConditionDTO getPrePostConditionByName(String className, String conditionName) {
+        return null;
+    }
+
+    public OperationDTO getOperationByName(String className, String operationName) {
+        return null;
+    }
+
+    public AttributeDTO getAttributeByName(String className, String attributeName) {
+        return null;
+    }
 }
