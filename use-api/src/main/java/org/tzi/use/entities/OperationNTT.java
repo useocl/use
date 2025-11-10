4713e1dffbb77d4535e289a077d9c95b6f9b55ac
husakki <hueschromo@gmail.com>
2025-11-10 20:12:40 +0100

    operations and attributes

--- use-api/src/main/java/org/tzi/use/entities/OperationNTT.java
+++ use-api/src/main/java/org/tzi/use/entities/OperationNTT.java
@@ -1,16 +1,19 @@
-package org.tzi.use.DTO;
+package org.tzi.use.entities;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-import lombok.AllArgsConstructor;
+import org.springframework.data.mongodb.core.mapping.Document;
 
+@Document("operation")
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
-public class OperationDTO {
+public class OperationNTT {
 
     private String operationName;
     private String[][] parameter;
     private String returnType;
 
 }
+
