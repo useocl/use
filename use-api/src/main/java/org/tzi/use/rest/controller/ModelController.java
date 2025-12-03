f1c92be4cc79c4b83885de4067a9596b0583b73f
husakki <hueschromo@gmail.com>
2025-12-03 16:05:51 +0100

    get endpoints better hateoas

--- use-api/src/main/java/org/tzi/use/rest/controller/ModelController.java
+++ use-api/src/main/java/org/tzi/use/rest/controller/ModelController.java
@@ -10,8 +10,8 @@ import org.tzi.use.DTO.*;
 import org.tzi.use.api.UseApiException;
 import org.tzi.use.rest.services.ModelService;
 
+import java.util.ArrayList;
 import java.util.List;
-import java.util.stream.Collectors;
 
 import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
 import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn;
@@ -37,13 +37,12 @@ public class ModelController {
      * @return The model with HATEOAS links
      */
     @GetMapping("/model/{modelName}")
-    public ResponseEntity<EntityModel<ModelDTO>> getModelByName(@PathVariable String modelName) {
+    public ResponseEntity<EntityModel<ModelDTO>> getModelByName(@PathVariable String modelName) throws UseApiException {
         ModelDTO modelDTO = modelService.getModelByName(modelName);
 
         EntityModel<ModelDTO> entityModel = EntityModel.of(modelDTO);
 
         entityModel.add(linkTo(methodOn(ModelController.class).getModelByName(modelName)).withSelfRel());
-        entityModel.add(linkTo(methodOn(ModelController.class).getAllModels()).withRel("models"));
         entityModel.add(linkTo(methodOn(ModelController.class).getClasses(modelName)).withRel("classes"));
         entityModel.add(linkTo(methodOn(ModelController.class).getAssociations(modelName)).withRel("associations"));
         entityModel.add(linkTo(methodOn(ModelController.class).getInvariants(modelName)).withRel("invariants"));
@@ -53,110 +52,73 @@ public class ModelController {
     }
 
     @GetMapping("/model/{modelName}/association/{associationName}")
-    public ResponseEntity<EntityModel<AssociationDTO>> getModelAssociationByName(@PathVariable String modelName, @PathVariable String associationName) {
+    public ResponseEntity<EntityModel<AssociationDTO>> getModelAssociationByName(@PathVariable String modelName, @PathVariable String associationName) throws UseApiException {
         AssociationDTO association = modelService.getAssociationByName(modelName, associationName);
+
         EntityModel<AssociationDTO> entityModel = EntityModel.of(association);
+
         entityModel.add(linkTo(methodOn(ModelController.class).getModelAssociationByName(modelName, associationName)).withSelfRel());
         entityModel.add(linkTo(methodOn(ModelController.class).getModelByName(modelName)).withRel("model"));
+        entityModel.add(linkTo(methodOn(ModelController.class).getAssociations(modelName)).withRel("associations"));
+
         return new ResponseEntity<>(entityModel, HttpStatus.OK);
     }
 
     @GetMapping("/model/{modelName}/invariant/{invariantName}")
-    public ResponseEntity<EntityModel<InvariantDTO>> getModelInvariantByName(@PathVariable String modelName, @PathVariable String invariantName) {
+    public ResponseEntity<EntityModel<InvariantDTO>> getModelInvariantByName(@PathVariable String modelName, @PathVariable String invariantName) throws UseApiException {
         InvariantDTO invariant = modelService.getInvariantByName(modelName, invariantName);
+
         EntityModel<InvariantDTO> entityModel = EntityModel.of(invariant);
+
         entityModel.add(linkTo(methodOn(ModelController.class).getModelInvariantByName(modelName, invariantName)).withSelfRel());
         entityModel.add(linkTo(methodOn(ModelController.class).getModelByName(modelName)).withRel("model"));
+        entityModel.add(linkTo(methodOn(ModelController.class).getInvariants(modelName)).withRel("invariants"));
+
         return new ResponseEntity<>(entityModel, HttpStatus.OK);
     }
 
-    /**
-     * Retrieve all models
-     *
-     * @return List of all models with HATEOAS links
-     */
     @GetMapping("/models")
-    public ResponseEntity<CollectionModel<EntityModel<ModelDTO>>> getAllModels() {
+    public ResponseEntity<CollectionModel<EntityModel<ModelDTO>>> getAllModels() throws UseApiException {
         List<ModelDTO> models = modelService.getAllModels();
 
-        // Convert each model to an EntityModel with links
-        List<EntityModel<ModelDTO>> modelEntities = models.stream().map(model -> {
-            EntityModel<ModelDTO> entityModel = EntityModel.of(model);
+        List<EntityModel<ModelDTO>> modelEntities = new ArrayList<>();
+        for (ModelDTO modelDTO : models) {
+            EntityModel<ModelDTO> entityModel = EntityModel.of(modelDTO);
+            entityModel.add(linkTo(methodOn(ModelController.class).getModelByName(modelDTO.getName())).withSelfRel());
+            entityModel.add(linkTo(methodOn(ModelController.class).getClasses(modelDTO.getName())).withRel("classes"));
+            entityModel.add(linkTo(methodOn(ModelController.class).getAssociations(modelDTO.getName())).withRel("associations"));
+            entityModel.add(linkTo(methodOn(ModelController.class).getInvariants(modelDTO.getName())).withRel("invariants"));
 
-            // Add self link
-            entityModel.add(linkTo(methodOn(ModelController.class).getModelByName(model.getName())).withSelfRel());
+            modelEntities.add(entityModel);
+        }
 
-            // Add classes link
-            entityModel.add(linkTo(methodOn(ModelController.class).getClasses(model.getName())).withRel("classes"));
-
-            // Add associations link
-            entityModel.add(linkTo(methodOn(ModelController.class).getAssociations(model.getName())).withRel("associations"));
-
-            // Add invariants link
-            entityModel.add(linkTo(methodOn(ModelController.class).getInvariants(model.getName())).withRel("invariants"));
-
-
-            return entityModel;
-        }).collect(Collectors.toList());
-
-        // Create a CollectionModel (container of EntityModels)
         CollectionModel<EntityModel<ModelDTO>> collectionModel = CollectionModel.of(modelEntities);
-
-        // Add link to this collection
         collectionModel.add(linkTo(methodOn(ModelController.class).getAllModels()).withSelfRel());
-
-        // Add link to create a new model
-        collectionModel.add(linkTo(methodOn(ModelController.class).createModel(null)).withRel("create-model"));
+        collectionModel.add(linkTo(methodOn(ModelController.class).createModel(null)).withRel("create model"));
 
 
         return new ResponseEntity<>(collectionModel, HttpStatus.OK);
     }
 
-    /**
-     * Placeholder method to retrieve all classes in a model
-     */
+
     @GetMapping("/model/{modelName}/classes")
-    public ResponseEntity<?> getClasses(@PathVariable String modelName) {
+    public ResponseEntity<?> getClasses(@PathVariable String modelName) throws UseApiException {
         List<ClassDTO> modelClasses = modelService.getModelClasses(modelName);
 
-        // Convert each class to an EntityModel with links
-        List<EntityModel<ClassDTO>> classEntities = modelClasses.stream().map(classDTO -> {
-            EntityModel<ClassDTO> entityModel = EntityModel.of(classDTO);
+        List<EntityModel<ClassDTO>> classEntities = new ArrayList<>();
+        for (ClassDTO classOfModelName : modelClasses) {
+            EntityModel<ClassDTO> entityModel = EntityModel.of(classOfModelName);
 
-            // Link to the parent model
-            entityModel.add(linkTo(methodOn(ModelController.class).getModelByName(modelName)).withRel("model"));
-
-            // Link to this classes collection (self)
             entityModel.add(linkTo(methodOn(ModelController.class).getClasses(modelName)).withSelfRel());
+            entityModel.add(linkTo(methodOn(ClassController.class).getClassByName(modelName, null)).withRel("class by name"));
 
-            // Link to create another class
-            try {
-                entityModel.add(linkTo(methodOn(ModelController.class).createClass(modelName, null)).withRel("create-class"));
-            } catch (UseApiException e) {
-                throw new RuntimeException(e);
-            }
-
-            // Link to associations
-            entityModel.add(linkTo(methodOn(ModelController.class).getAssociations(modelName)).withRel("associations"));
-
-            // Link to invariants
-            entityModel.add(linkTo(methodOn(ModelController.class).getInvariants(modelName)).withRel("invariants"));
-
-            return entityModel;
-        }).collect(Collectors.toList());
-
-        // Create a CollectionModel
-        CollectionModel<EntityModel<ClassDTO>> collectionModel = CollectionModel.of(classEntities);
-
-        // Add self-link to the collection
-        collectionModel.add(linkTo(methodOn(ModelController.class).getClasses(modelName)).withSelfRel());
-
-        // Add link to create a new class
-        try {
-            collectionModel.add(linkTo(methodOn(ModelController.class).createClass(modelName, null)).withRel("create-class"));
-        } catch (UseApiException e) {
-            throw new RuntimeException(e);
+            classEntities.add(entityModel);
         }
+
+        CollectionModel<EntityModel<ClassDTO>> collectionModel = CollectionModel.of(classEntities);
+        collectionModel.add(linkTo(methodOn(ModelController.class).getClasses(modelName)).withSelfRel());
+        collectionModel.add(linkTo(methodOn(ModelController.class).getModelByName(modelName)).withRel("model"));
+        collectionModel.add(linkTo(methodOn(ModelController.class).createClass(modelName, null)).withRel("create class"));
 
         return new ResponseEntity<>(collectionModel, HttpStatus.OK);
     }
@@ -166,95 +128,85 @@ public class ModelController {
      * Placeholder method to retrieve all associations in a model
      */
     @GetMapping("/model/{modelName}/associations")
-    public ResponseEntity<CollectionModel<EntityModel<AssociationDTO>>> getAssociations(@PathVariable String modelName) {
+    public ResponseEntity<CollectionModel<EntityModel<AssociationDTO>>> getAssociations(@PathVariable String modelName) throws UseApiException {
         List<AssociationDTO> associations = modelService.getModelAssociations(modelName);
 
-        List<EntityModel<AssociationDTO>> associationEntities = associations.stream().map(association -> {
-            EntityModel<AssociationDTO> entityModel = EntityModel.of(association);
-            entityModel.add(linkTo(methodOn(ModelController.class).getModelAssociationByName(modelName, association.getAssociationName())).withSelfRel());
-            entityModel.add(linkTo(methodOn(ModelController.class).getModelByName(modelName)).withRel("model"));
-            return entityModel;
-        }).collect(Collectors.toList());
+        List<EntityModel<AssociationDTO>> associationEntities = new ArrayList<>();
+        for (AssociationDTO associationDTO : associations) {
+            EntityModel<AssociationDTO> entityModel = EntityModel.of(associationDTO);
+
+            entityModel.add(linkTo(methodOn(ModelController.class).getModelAssociationByName(modelName, associationDTO.getAssociationName())).withSelfRel());
+            associationEntities.add(entityModel);
+        }
 
         CollectionModel<EntityModel<AssociationDTO>> collectionModel = CollectionModel.of(associationEntities);
 
         collectionModel.add(linkTo(methodOn(ModelController.class).getAssociations(modelName)).withSelfRel());
+        collectionModel.add(linkTo(methodOn(ModelController.class).getModelByName(modelName)).withRel("model"));
+        collectionModel.add(linkTo(methodOn(ModelController.class).createAssociation(modelName, null)).withRel("create association"));
+
 
         return new ResponseEntity<>(collectionModel, HttpStatus.OK);
     }
 
-    /**
-     * Retrieve all invariants in a model.
-     *
-     * @param modelName The name of the model.
-     * @return A collection of invariants with HATEOAS links.
-     */
+
     @GetMapping("/model/{modelName}/invariants")
-    public ResponseEntity<CollectionModel<EntityModel<InvariantDTO>>> getInvariants(@PathVariable String modelName) {
+    public ResponseEntity<CollectionModel<EntityModel<InvariantDTO>>> getInvariants(@PathVariable String modelName) throws UseApiException {
         List<InvariantDTO> invariants = modelService.getModelInvariants(modelName);
 
-        List<EntityModel<InvariantDTO>> invariantEntities = invariants.stream().map(invariant -> {
-            EntityModel<InvariantDTO> entityModel = EntityModel.of(invariant);
 
-            // Link to the parent model
-            entityModel.add(linkTo(methodOn(ModelController.class).getModelByName(modelName)).withRel("model"));
+        List<EntityModel<InvariantDTO>> invariantEntities = new ArrayList<>();
+        for (InvariantDTO invariantDTO : invariants) {
+            EntityModel<InvariantDTO> entityModel = EntityModel.of(invariantDTO);
 
-            // Self-link to this collection
             entityModel.add(linkTo(methodOn(ModelController.class).getInvariants(modelName)).withSelfRel());
 
-            // Link to classes
-            entityModel.add(linkTo(methodOn(ModelController.class).getClasses(modelName)).withRel("classes"));
-
-            // Link to associations
-            entityModel.add(linkTo(methodOn(ModelController.class).getAssociations(modelName)).withRel("associations"));
-
-            return entityModel;
-        }).collect(Collectors.toList());
+            invariantEntities.add(entityModel);
+        }
 
         CollectionModel<EntityModel<InvariantDTO>> collectionModel = CollectionModel.of(invariantEntities);
 
-        // Self-link for the collection
         collectionModel.add(linkTo(methodOn(ModelController.class).getInvariants(modelName)).withSelfRel());
+        collectionModel.add(linkTo(methodOn(ModelController.class).getModelByName(modelName)).withRel("model"));
+        collectionModel.add(linkTo(methodOn(ModelController.class).createInvariant(modelName,null,null)).withRel("create invariant"));
+
 
         return new ResponseEntity<>(collectionModel, HttpStatus.OK);
     }
 
     @GetMapping("/model/{modelName}/prepostconditions")
-    public ResponseEntity<CollectionModel<EntityModel<PrePostConditionDTO>>> getPrePostConditions(@PathVariable String modelName) {
+    public ResponseEntity<CollectionModel<EntityModel<PrePostConditionDTO>>> getPrePostConditions(@PathVariable String modelName) throws UseApiException {
         List<PrePostConditionDTO> prePostConditions = modelService.getModelPrePostConditions(modelName);
 
-        List<EntityModel<PrePostConditionDTO>> prePostConditionEntities = prePostConditions.stream().map(prePostCondition -> {
-            EntityModel<PrePostConditionDTO> entityModel = EntityModel.of(prePostCondition);
 
-            // Link to the parent model
-            entityModel.add(linkTo(methodOn(ModelController.class).getModelByName(modelName)).withRel("model"));
+        List<EntityModel<PrePostConditionDTO>> prePostConditionEntities = new ArrayList<>();
+        for (PrePostConditionDTO prePostConditionDTO : prePostConditions) {
+            EntityModel<PrePostConditionDTO> entityModel = EntityModel.of(prePostConditionDTO);
 
-            // Self-link to this collection
             entityModel.add(linkTo(methodOn(ModelController.class).getPrePostConditions(modelName)).withSelfRel());
 
-            // Link to classes
-            entityModel.add(linkTo(methodOn(ModelController.class).getClasses(modelName)).withRel("classes"));
-
-            // Link to associations
-            entityModel.add(linkTo(methodOn(ModelController.class).getAssociations(modelName)).withRel("associations"));
-
-            return entityModel;
-        }).collect(Collectors.toList());
+            prePostConditionEntities.add(entityModel);
+        }
 
         CollectionModel<EntityModel<PrePostConditionDTO>> collectionModel = CollectionModel.of(prePostConditionEntities);
 
-        // Self-link for the collection
         collectionModel.add(linkTo(methodOn(ModelController.class).getPrePostConditions(modelName)).withSelfRel());
+        collectionModel.add(linkTo(methodOn(ModelController.class).getModelByName(modelName)).withRel("model"));
+        collectionModel.add(linkTo(methodOn(ModelController.class).createprepostcondition(modelName,null,null)).withRel("create prepostcondition"));
 
         return new ResponseEntity<>(collectionModel, HttpStatus.OK);
     }
 
     @GetMapping("/model/{modelName}/prepostcondition/{prePostConditionName}")
-    public ResponseEntity<EntityModel<PrePostConditionDTO>> getModelPrePostCondByName(@PathVariable String modelName, @PathVariable String prePostConditionName) {
+    public ResponseEntity<EntityModel<PrePostConditionDTO>> getModelPrePostCondByName(@PathVariable String modelName, @PathVariable String prePostConditionName) throws UseApiException {
         PrePostConditionDTO prePostCondition = modelService.getPrePostConditionByName(modelName, prePostConditionName);
+
         EntityModel<PrePostConditionDTO> entityModel = EntityModel.of(prePostCondition);
+
         entityModel.add(linkTo(methodOn(ModelController.class).getModelPrePostCondByName(modelName, prePostConditionName)).withSelfRel());
         entityModel.add(linkTo(methodOn(ModelController.class).getModelByName(modelName)).withRel("model"));
+        entityModel.add(linkTo(methodOn(ModelController.class).getPrePostConditions(modelName)).withRel("prePostConditions"));
+
         return new ResponseEntity<>(entityModel, HttpStatus.OK);
     }
 
@@ -269,7 +221,7 @@ public class ModelController {
      * @return The created model with HATEOAS links
      */
     @PostMapping("/model")
-    public ResponseEntity<EntityModel<ModelDTO>> createModel(@RequestBody ModelDTO modelDTO) {
+    public ResponseEntity<EntityModel<ModelDTO>> createModel(@RequestBody ModelDTO modelDTO) throws UseApiException {
         ModelDTO createdModel = modelService.createModel(modelDTO);
 
         // Create an EntityModel (HATEOAS) with the response
